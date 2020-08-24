0f539c4f91cab7086bac3acb63e3d62ad02403e0
Magdalena Lipińska <szymczyk.meg@gmail.com>
2020-08-24 11:06:53 +0200

    Add sum

--- Shopping_List1.py
+++ Shopping_List1.py
@@ -4,3 +4,10 @@ shopping_dict = {
     "warzywniak": ["marchew", "seler", "rukola"]
 }
 print(shopping_dict)
+
+for keys, values in shopping_dict.items():
+    print(f"Idę do {keys.title()} i kupuję tam: {str(values).title()}")
+
+produkty = sum(shopping_dict.values(), [])
+print(f"W sumie kupuję {len(produkty)} produktów")
+   
