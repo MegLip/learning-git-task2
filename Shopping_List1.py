35e53c43e0436b19bedb58b324810c90e417d5d8
Magdalena Lipińska <szymczyk.meg@gmail.com>
2020-08-24 10:44:19 +0200

    Init project

---
+++ Shopping_List1.py
@@ -0,0 +1,10 @@
+print("Lista zakupów")
+shopping_dict = {
+    "piekarnia": ["chleb", "bułki", "pączek"],
+    "warzywniak": ["marchew", "seler", "rukola"]
+}
+for keys, values in shopping_dict.items():
+    print(f"Idę do {keys.title()} i kupuję tam: {str(values).title()}")
+
+produkty = sum(shopping_dict.values(), [])
+print(f"W sumie kupuję {len(produkty)} produktów")
