ec16f31f2444070865fe45da61833c9ffcad1436
Magdalena Lipińska <szymczyk.meg@gmail.com>
2020-08-25 01:53:17 +0200

    Add ulubione

--- Shopping_List1.py
+++ Shopping_List1.py
@@ -1,4 +1,4 @@
-print("Lista codziennych zakupów")
+print("Lista codziennych zakupów") #1 commit Zmiana nazwy
 shopping_dict = {
     "piekarnia": ["chleb", "bułki", "pączek"],
     "warzywniak": ["marchew", "seler", "rukola"]
@@ -10,3 +10,5 @@ for keys, values in shopping_dict.items():
 
 produkty = sum(shopping_dict.values(), [])
 print(f"W sumie kupuję {len(produkty)} produktów")
+
+print(f"Mój ulubiony sklep to: {keys}, a najbardziej lubię jeść: {values[0:3]}") #2 commit ulubione
